3fc8ff97617240af06fff73ebff2474ed24d0c9b
Angalexik <58977805+Angalexik@users.noreply.github.com>
2024-12-03 10:21:41 +0100

    Day3: clean up

--- Day3.fs
+++ Day3.fs
@@ -4,38 +4,30 @@ open AdventUtils
 open System
 open System.Text.RegularExpressions
 
-type Mul = { Index: int; Num1: int; Num2: int }
+type Mul = { Num1: int; Num2: int }
 
 type Instrution =
-    | Do of int
-    | Dont of int
+    | Do
+    | Dont
     | Mul of Mul
-
-let index =
-    function
-    | Do(idx) -> idx
-    | Dont(idx) -> idx
-    | Mul({ Index = idx }) -> idx
 
 let parse1 input =
     Regex.Matches(input, @"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)")
     |> Seq.map (fun matsch -> (matsch.Groups.[1].Value |> int, matsch.Groups[2].Value |> int))
 
 let parse2 input =
-    let dontIdxs = Regex.Matches(input, @"don't\(\)") |> Seq.map (_.Index >> Dont)
-    let doIdxs = Regex.Matches(input, @"do\(\)") |> Seq.map (_.Index >> Do)
+    let toInstrution (matsch: Match) =
+        match matsch.Value with
+        | "do()" -> Do
+        | "don't()" -> Dont
+        | _ ->
+            Mul(
+                { Num1 = matsch.Groups.[1].Value |> int
+                  Num2 = matsch.Groups.[2].Value |> int }
+            )
 
-    let muls =
-        Regex.Matches(input, @"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)")
-        |> Seq.map (
-            (fun matsch ->
-                { Index = matsch.Index
-                  Num1 = matsch.Groups.[1].Value |> int
-                  Num2 = matsch.Groups.[2].Value |> int })
-            >> Mul
-        )
-
-    dontIdxs |> Seq.append doIdxs |> Seq.append muls |> Seq.sortBy index
+    Regex.Matches(input, @"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)|do\(\)|don't\(\)")
+    |> Seq.map toInstrution
 
 let solve1 input =
     input |> Seq.sumBy (fun (num1, num2) -> num1 * num2)
@@ -43,8 +35,8 @@ let solve1 input =
 let solve2 instructions =
     let folder (enabled, total) curr =
         match curr with
-        | Do(_) -> (true, total)
-        | Dont(_) -> (false, total)
+        | Do -> (true, total)
+        | Dont -> (false, total)
         | Mul({ Num1 = n1; Num2 = n2 }) -> (enabled, if enabled then total + (n1 * n2) else total)
 
     instructions |> Seq.fold folder (true, 0) |> snd
