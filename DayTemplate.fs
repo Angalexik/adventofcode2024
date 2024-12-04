67890aef910e984e0dfd0299b9c60a277721c0ba
Angalexik <58977805+Angalexik@users.noreply.github.com>
2024-12-04 10:45:20 +0100

    Add template file

---
+++ DayTemplate.fs
@@ -0,0 +1,20 @@
+module Day0
+
+open AdventUtils
+open System
+
+let solve1 input =
+    ()
+
+let solve2 input =
+    ()
+
+let test () =
+    let solution = (dayTestInputs 0).[0] |> solve1
+    printfn $"{solution}"
+
+let part1 () =
+    printfn $"Part 1: {dayInput 0 |> solve1}"
+
+let part2 () =
+    printfn $"Part 2: {dayInput 0 |> solve2}"
