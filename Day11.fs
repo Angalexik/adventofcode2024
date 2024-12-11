cc988e68eb6275812d3d7a92995675d25797e6eb
Angalexik <58977805+Angalexik@users.noreply.github.com>
2024-12-11 22:52:21 +0100

    Day 11, Part 2: (Time: 1:19:37)
    I guess you could say, I got this one _in the bag_! *badum-tss*

--- Day11.fs
+++ Day11.fs
@@ -5,37 +5,76 @@ open System
 open Utils.Text
 open Utils.Func
 
-let applyRules stones =
+type Bag<'a when 'a: comparison> = Map<'a, uint64>
+
+module Bag =
+    let empty: 'a Bag = Map.empty
+
+    let isEmpty (bag: 'a Bag) = bag = Map.empty
+
+    let addMany value count (bag: 'a Bag) : 'a Bag =
+        if count < 0UL then
+            invalidArg "count" "Must not be a negative nmber"
+
+        Map.change
+            value
+            (fun old ->
+                match old with
+                | None -> Some(uint64 count)
+                | Some(oldCount) -> Some(uint64 count + oldCount))
+            bag
+
+    let removeMany value count (bag: 'a Bag) : 'a Bag =
+        if count < 0UL then
+            invalidArg "count" "Must not be a negative nmber"
+
+        Map.change
+            value
+            (Option.bind (fun oldCount ->
+                if oldCount <= uint64 count then
+                    None
+                else
+                    Some(oldCount - uint64 count)))
+            bag
+
+    let add value (bag: 'a Bag) : 'a Bag = addMany value 1UL bag
+
+    let remove value (bag: 'a Bag) : 'a Bag = removeMany value 1UL bag
+
+    let count (bag: 'a Bag) = Map.values bag |> Seq.sum
+
+    let fromSeq (seq: 'a seq) =
+        Seq.fold (fun acc curr -> add curr acc) empty seq
+
+let applyRules (stones: int64 Bag) =
     let splitNum number =
         let str = number.ToString()
         let length = str.Length / 2
         let firstHalf = str.Substring(0, length) |> int64
         let secondHalf = str.Substring(length) |> int64
-        [ firstHalf; secondHalf ]
+        (firstHalf, secondHalf)
 
-    List.collect
-        (fun stone ->
-            match stone with
-            | 0L -> [ 1L ]
-            | even when (stone.ToString().Length % 2 = 0) -> splitNum even
-            | n -> [ n * 2024L ])
-        stones
+    stones
+    |> Map.toSeq
+    |> Seq.fold
+        (fun acc (stone, count) ->
+            acc
+            |> match stone with
+               | 0L -> Bag.addMany 1L count
+               | even when (stone.ToString().Length % 2 = 0) ->
+                   let (firstStone, secondStone) = splitNum even
+                   Bag.addMany firstStone count >> Bag.addMany secondStone count
+               | n -> Bag.addMany (n * 2024L) count)
+        Bag.empty
 
 let parse1 input =
-    split " " input |> Array.map int64 |> Array.toList
+    split " " input |> Array.map int64 |> Bag.fromSeq
 
 let solve1 input =
-    (repeat 25 applyRules) input |> List.length
+    (repeat 25 applyRules) input |> Bag.count
 
 let solve2 input =
-    let mutable start = input
-
-    [ 1..75 ]
-    |> Seq.iter (fun i ->
-        printfn $"{i}"
-        start <- applyRules start)
-
-    List.length start
+    (repeat 75 applyRules) input |> Bag.count
 
 let test () =
     let inputs = (dayTestInputs 11)
@@ -43,8 +82,6 @@ let test () =
     dbg test1 |> ignore
     let test2 = inputs.[1] |> parse1 |> (repeat 6 applyRules)
     dbg test2 |> ignore
-// let solution = (dayTestInputs 11).[0] |> solve1
-// printfn $"{solution}"
 
 let part1 () =
     printfn $"Part 1: {dayInput 11 |> parse1 |> solve1}"
