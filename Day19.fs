af3ad121905e8ed45ba27a91d8293e3f5d3576fb
Angalexik <58977805+Angalexik@users.noreply.github.com>
2025-02-02 11:48:51 +0100

    Day 19 part 2

--- Day19.fs
+++ Day19.fs
@@ -26,6 +26,34 @@ let possible towels target =
 
     loop [ target ] Set.empty
 
+// Absolutely incomprehensible
+let ways towels target =
+    let mutable cache = Map.ofList [ ("", 0UL) ]
+
+    let rec loop (partial: string) =
+        match cache.TryFind partial with
+        | Some v -> v
+        | None ->
+            let plusOne =
+                if towels |> List.exists (fun e -> e = partial) then
+                    1UL
+                else
+                    0UL
+
+            let noEnd =
+                towels
+                |> List.choose (fun (t: string) ->
+                    if partial.EndsWith(t) then
+                        Some <| partial.Substring(0, partial.Length - t.Length)
+                    else
+                        None)
+
+            let ret = noEnd |> List.map loop |> List.sum |> (+) plusOne
+            cache <- Map.add partial ret cache
+            ret
+
+    loop target
+
 let parse1 input =
     let (towels, seqs) = splitOnce "\n\n" input
     let towels = towels |> split ", " |> Array.toList
@@ -35,14 +63,15 @@ let parse1 input =
 let solve1 (towels, seqs) =
     seqs |> Array.filter (possible towels) |> Array.length
 
-let solve2 input = ()
+let solve2 (towels, seqs) =
+    seqs |> Array.map (ways towels) |> Array.sum
 
 let test () =
-    let solution = (dayTestInputs 19).[0] |> parse1 |> solve1
-    printfn $"{solution}"
+    let solution = (dayTestInputs 19).[0] |> parse1 |> solve2
+    printfn "%A" solution
 
 let part1 () =
     printfn $"Part 1: {dayInput 19 |> parse1 |> solve1}"
 
 let part2 () =
-    printfn $"Part 2: {dayInput 19 |> solve2}"
+    printfn $"Part 2: {dayInput 19 |> parse1 |> solve2}"
